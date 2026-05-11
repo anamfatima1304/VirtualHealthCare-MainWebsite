49b0a84c99f108fbb30f7b6bef2deee77748db57
Anam Fatima <anam.fatima1304@gmail.com>
2026-05-11 13:09:00 +0500

    Added nip.io certificate

--- src/app/Data/doctors.service.ts
+++ src/app/Data/doctors.service.ts
@@ -8,7 +8,7 @@ import { Doctor } from '../Shared/Interfaces/Doctor.interface';
   providedIn: 'root'
 })
 export class DoctorsService {
-  private apiUrl = 'http://20.13.9.186/hospital/api/doctors';
+  private apiUrl = 'http://20.13.9.186.nip.io/hospital/api/doctors';
   
   // Keep local data as fallback
   private localDoctors: Doctor[] = [
